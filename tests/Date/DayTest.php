622bed37288eea2be0df01527a93233cc0df1d43
Alexandru Guzinschi <alex@gentle.ro>
2016-08-02 16:55:13 +0300

    updated tests for Day

--- tests/Date/DayTest.php
+++ tests/Date/DayTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2016 Alexandru Guzinschi <alex@gentle.ro>
+ * Copyright 2016 Alexandru Guzinschi <alex@gentle.ro>.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -10,6 +10,9 @@ namespace Gentle\Embeddable\Test\Date;
 use Gentle\Embeddable\Date\Day;
 use Gentle\Embeddable\Test\TestCase;
 
+/**
+ * @author Alexandru Guzinschi <alex@gentle.ro>
+ */
 class DayTest extends TestCase
 {
     /**
@@ -21,6 +24,42 @@ class DayTest extends TestCase
     {
         $day = new Day($value);
         $this->assertInstanceOf('Gentle\Embeddable\Date\Day', $day);
+    }
+
+    /**
+     * @param string|int $value
+     *
+     * @dataProvider invalidDayProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInstantiateError($value)
+    {
+        new Day($value);
+    }
+
+    /**
+     * @param string|int $value
+     *
+     * @dataProvider invalidDayRangeProvider
+     * @expectedException \OutOfRangeException
+     */
+    public function testDayRangeError($value)
+    {
+        new Day($value);
+    }
+
+    /**
+     * @param string|int $value
+     *
+     * @dataProvider validDayProvider
+     */
+    public function testEqualityByValue($value)
+    {
+        $day1 = new Day($value);
+        $day2 = new Day($value);
+
+        $this->assertTrue($day1->equals($day2));
+        $this->assertNotSame($day1, $day2);
     }
 
     /**
